ae0b2de05c8e6a000bd62981b0d8d95dd720ca23
EmpingRenyah <viha.el2629@gmail.com>
2025-05-26 10:54:30 +0700

    feat: add Buyer and password
    adding buyer class and adding password string to user class

--- src/main/java/app/modules/User.java
+++ src/main/java/app/modules/User.java
@@ -3,9 +3,11 @@ package app.modules;
 public class User {
 	private String username;
 	private String role;
+	private String password;
 	
-	public User(String username, String role) {
+	public User(String username, String password, String role) {
 		this.username = username;
+		this.password = password;
 		this.role = role;
 	}
 }
